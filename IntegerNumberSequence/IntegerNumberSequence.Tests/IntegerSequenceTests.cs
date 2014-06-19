9d1d46f113506a49f250d983892580e2021a4d51
DJ <omegaman912000-uk@yahoo.co.uk>
2014-06-19 22:58:30 +0100

    DJ: Integer Number Sequence Kata.

---
+++ IntegerNumberSequence/IntegerNumberSequence.Tests/IntegerSequenceTests.cs
@@ -0,0 +1,123 @@
+﻿using NUnit.Framework;
+
+namespace IntegerNumberSequence.Tests
+{
+    /*
+    Code Kata to process an integer sequence to determine the following statistics:
+
+    Minimum Value
+    Maximum Value
+    Average Value
+    E.g. For a integer sequence "6, 9, 15, -2, 92, 11" we have the following answers:
+
+    Minimum Value: -2
+    Maximum Value: 92
+    Average Value: 21.83
+    */
+    public class IntegerSequenceTests
+    {
+        private int[] _numbers = {6, 9, 15, -2, 92, 11};
+        
+        [Test]
+        public void Should_Return_Minium_Value()
+        {
+            var integerSequence = new IntegerSequence(_numbers);
+
+            integerSequence.FindExtremaValues();
+            var foundMiniumValue = integerSequence.Min;
+
+            Assert.That(foundMiniumValue, Is.EqualTo(-2));
+        }
+
+        [Test]
+        public void Should_When_Finding_Minium_Value_If_Array_Is_Empty_Min_Value_Minus_One()
+        {
+            _numbers = new int[] { };
+
+            var integerSequence = new IntegerSequence(_numbers);
+            integerSequence.FindExtremaValues();
+            var foundMiniumValue = integerSequence.Min;
+
+            Assert.That(foundMiniumValue, Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void Should_When_Finding_Minium_Value_If_Array_Is_Null_Return_Minus_One()
+        {
+            _numbers = null;
+            var integerSequence = new IntegerSequence(_numbers);
+
+            integerSequence.FindExtremaValues();
+            var foundMiniumValue = integerSequence.Min;
+
+            Assert.That(foundMiniumValue, Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void Should_Return_Maxium_Value()
+        {
+            var integerSequence = new IntegerSequence(_numbers);
+
+            integerSequence.FindExtremaValues();
+            var foundMaximumValue = integerSequence.Max;
+
+            Assert.That(foundMaximumValue, Is.EqualTo(92));
+        }
+
+        [Test]
+        public void Should_When_Finding_Maximum_Value_If_Array_Is_Empty_Return_Minus_One()
+        {
+            _numbers = new int[] { };
+            var integerSequence = new IntegerSequence(_numbers);
+
+            integerSequence.FindExtremaValues();
+            var foundMaximumValue = integerSequence.Max;
+
+            Assert.That(foundMaximumValue, Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void Should_When_Finding_Maximum_Value_If_Array_Is_Null_Return_Minus_One()
+        {
+            _numbers = null;
+            var integerSequence = new IntegerSequence(_numbers);
+
+            integerSequence.FindExtremaValues();
+            var foundMaximumValue = integerSequence.Max;
+
+            Assert.That(foundMaximumValue, Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void Should_Return_The_Average_Value()
+        {
+            var integerSequence = new IntegerSequence(_numbers);
+
+            var foundAverageValue = integerSequence.FindAverageValue();
+
+            Assert.That(foundAverageValue, Is.EqualTo(21.83));
+        }
+
+        [Test]
+        public void Should_When_Finding_Average_Value_If_Array_Is_Empty_Return_Minus_One()
+        {
+            _numbers = new int[] { };
+
+            var integerSequence = new IntegerSequence(_numbers);
+            var foundAverageValue = integerSequence.FindAverageValue();
+
+            Assert.That(foundAverageValue, Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void Should_When_Finding_Average_Value_If_Array_Is_Null_Return_Minus_One()
+        {
+            _numbers = null;
+
+            var integerSequence = new IntegerSequence(_numbers);
+            var foundAverageValue = integerSequence.FindAverageValue();
+
+            Assert.That(foundAverageValue, Is.EqualTo(-1));
+        }
+    }
+}
---
+++ IntegerNumberSequence/IntegerNumberSequence/IntegerSequence.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace IntegerNumberSequence
+{
+    public class IntegerSequence
+    {
+        private readonly int[] _numbers;
+
+        public IntegerSequence(int[] numbers)
+        {
+            this._numbers = numbers;
+            this.Max = -1;
+            this.Min = -1;
+        }
+
+        public int Max { get; set; }
+        public int Min { get; set; }
+
+        public void FindExtremaValues()
+        {
+            if (ArrayIsNotInitized()) return;
+            
+            Min = this._numbers[0];
+            Max = this._numbers[0];
+
+            for (var index = 0; index <= this._numbers.Length - 1; index++)
+            {
+                var currentElementValue = this._numbers[index];
+                if (Min >= currentElementValue)
+                {
+                    Min = currentElementValue;
+                }
+
+                if (Max <= currentElementValue)
+                {
+                    Max = currentElementValue;
+                }
+            }
+        }
+
+        private bool ArrayIsNotInitized()
+        {
+            return this._numbers == null || this._numbers.Length == 0;
+        }
+
+        public double FindAverageValue()
+        {
+            if (ArrayIsNotInitized()) return -1;
+            
+            double total = 0;
+            double arrayLegnth = this._numbers.Length;
+            
+            for (var index = 0; index <= arrayLegnth - 1; index++)
+            {
+                total += this._numbers[index];
+            }
+
+            return Math.Truncate((total / arrayLegnth) * 100) / 100;
+        }
+    }
+}
