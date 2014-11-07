b3a66f88424484ea437cec6420f0ea4468da0c2e
David Jacobs <david.jacobs@bglgroup.co.uk>
2014-11-07 14:59:48 +0000

    DJ: Prime Factor Code Kata (Rob Martin)

---
+++ Kata.PrimeFactors/Src/PrimeFactors/Kata.PrimeFactor.Tests/PrimeFactorTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Kata.PrimeFactor.Tests
+{
+    [TestFixture]
+    public class PrimeFactorTests
+    {
+        [Test]
+        public void Given_a_value_of_1_expect_argument_exception_with_error_message()
+        {
+            var primeFactors = new PrimeFactors();
+            const string expectedExceptionMessage = "1 cannot by factored";
+
+            var argumentException = Assert.Throws<ArgumentException>(() => primeFactors.Generate(1));
+            Assert.That(argumentException.Message, Is.EqualTo(expectedExceptionMessage));
+        }
+
+        [Test]
+        public void Given_a_value_of_2_expect_prime_factor_2()
+        {
+            var primeFactors = new PrimeFactors();
+
+            var listOfPrimeFactors = primeFactors.Generate(2);
+            var expected = new List<int> {2};
+
+            Assert.That(listOfPrimeFactors,Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Given_a_value_of_3_expect_prime_factor_3()
+        {
+            var primeFactors = new PrimeFactors();
+
+            var listOfPrimeFactors = primeFactors.Generate(3);
+            var expected = new List<int> { 3 };
+
+            Assert.That(listOfPrimeFactors, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Given_a_value_of_4_expect_prime_factors_2x2()
+        {
+            var primeFactors = new PrimeFactors();
+
+            var listOfPrimeFactors = primeFactors.Generate(4);
+            var expected = new List<int> {2,2};
+
+            Assert.That(listOfPrimeFactors, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Given_a_value_of_5_expect_prime_factor_5()
+        {
+            var primeFactors = new PrimeFactors();
+
+            var listOfPrimeFactors = primeFactors.Generate(5);
+            var expected = new List<int> { 5 };
+
+            Assert.That(listOfPrimeFactors, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Given_a_value_of_6_expect_prime_factors_2x3()
+        {
+            var primeFactors = new PrimeFactors();
+
+            var listOfPrimeFactors = primeFactors.Generate(6);
+            var expected = new List<int> { 2, 3 };
+
+            Assert.That(listOfPrimeFactors, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Given_a_value_of_7_expect_prime_factor_7()
+        {
+            var primeFactors = new PrimeFactors();
+
+            var listOfPrimeFactors = primeFactors.Generate(7);
+            var expected = new List<int> { 7 };
+
+            Assert.That(listOfPrimeFactors, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ Kata.PrimeFactors/Src/PrimeFactors/Kata.PrimeFactor.Tests/PrimeFactors.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Kata.PrimeFactor.Tests
+{
+    public interface IPrimeFactors
+    {
+        //List<int> Generate(int given);
+        List<int> GeneratePrime(int given);
+    }
+    
+    public class PrimeFactors : IPrimeFactors   
+    {
+        public List<int> Generate(int given)
+        {
+            if (given == 1)
+            {
+                throw new ArgumentException("1 cannot by factored");
+            }
+
+            var divider = 2;
+            var primeFactor = given;
+
+            var primeFactors = FindPrimeFactors(ref primeFactor, divider);
+            divider+=1;
+            primeFactors.AddRange(FindPrimeFactors(ref primeFactor, divider));
+
+            if (primeFactors.Count == 0)
+            {
+                //Its a prime number
+                primeFactors.Add(given);
+            }
+
+            primeFactors.Sort();
+            return primeFactors;
+        }
+
+        private List<int> FindPrimeFactors(ref int primeFactor, int divider)
+        {
+            var primeFactors = new List<int>();
+
+            while (primeFactor%divider == 0)
+            {
+                primeFactor /= divider;
+                if (primeFactor >= 1)
+                {
+                    primeFactors.Add(divider);
+                }
+            }
+            return primeFactors;
+        }
+
+
+        public List<int> GeneratePrime(int givenNumer)
+        {
+            var primefactores = new List<int>();
+            var primeFactor = givenNumer;
+            var divisor = 2;
+            while (CheckEven(primeFactor))
+            {
+             
+                primeFactor = givenNumer / divisor;
+                givenNumer = primeFactor;
+               primefactores.Add(divisor);
+            }
+            divisor++;
+            while (checkRemainderForOddNumbers(primeFactor))
+            {
+                primeFactor = givenNumer / divisor;
+                givenNumer = primeFactor;
+                primefactores.Add(divisor);
+            }
+            return primefactores;
+        }
+
+        private static bool CheckEven(int givenNumer)
+        {
+            return givenNumer%2 == 0;
+        }
+
+        private static bool checkRemainderForOddNumbers(int givenNumer)
+        {
+            return givenNumer % 3 == 0;
+        }
+    }
+}
